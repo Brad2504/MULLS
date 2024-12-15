3c33370c0e7cd10203f5198c18f096a865f58921
Brad2504 <157432373+Brad2504@users.noreply.github.com>
2024-12-15 15:52:36 -0700

    Update mullsRun.sh

--- mullsRun.sh
+++ mullsRun.sh
@@ -35,6 +35,6 @@ run_and_source "bash /mulls/script/tools/rosbag2pcd.sh /data /tmp/data/pcd '$poi
 run_and_source "bash /mulls/script/run_mulls_slam.sh /tmp/data $outputName $config"
 
 run_and_source "python3.8 /mulls/timestamp.py /tmp/data/result/timing_table_$outputName.txt /tmp/timestamp.txt"
-run_and_source "python3.8 /mulls/kitti_to_tum.py /tmp/data/result/pose_b_lo_$outputName.txt /tmp/timestamp.txt /results/$outputName.tum"
+run_and_source "python3.8 /mulls/kitti_to_tum.py /tmp/data/result/pose_b_lo_$outputName.txt /tmp/timestamp.txt /results/'$outputName'_mulls.tum"
 
 docker stop $containerID
